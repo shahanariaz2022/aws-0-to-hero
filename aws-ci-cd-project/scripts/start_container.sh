bc136cb686ee4bcc3af7f9419621537171407e33
shahanariaz2022 <110471537+shahanariaz2022@users.noreply.github.com>
2026-02-17 09:12:40 -0600

    Update start_container.sh

--- aws-ci-cd-project/scripts/start_container.sh
+++ aws-ci-cd-project/scripts/start_container.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull shahana2022/aws-ci-cd-project:latest
+# Run the Docker image as a container
+docker run -d -p 5000:5000 shahana2022/aws-ci-cd-project:latest
