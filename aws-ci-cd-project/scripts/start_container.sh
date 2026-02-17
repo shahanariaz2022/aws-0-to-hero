2db3dbc36d7c868b769e37a275fcad4d03f4efbc
shahanariaz2022 <110471537+shahanariaz2022@users.noreply.github.com>
2026-02-17 09:35:28 -0600

    Delete aws-ci-cd-project/scripts directory

--- aws-ci-cd-project/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull shahana2022/aws-ci-cd-project:latest
-# Run the Docker image as a container
-docker run -d -p 5000:5000 shahana2022/aws-ci-cd-project:latest
